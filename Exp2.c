6f982dd631ff595f442d018bd58eb3a2d49f5548
mawenbing <1390776845@qq.com>
2024-11-28 12:52:20 +0800

    3333

--- Exp2.c
+++ Exp2.c
@@ -3,6 +3,6 @@
 int main()
 {
     int a, b, c, d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z;
-    printf("a = ,b=,c=,d=,e=,f=,g=,h=,i=,j=,k=,l=,m=,n=,o=,p=,q=,r=,s=,t=,u=,v=,w=,x=,y=,z=;");
+    printf("a = ,b=,");
     printf("a = ");
 }
